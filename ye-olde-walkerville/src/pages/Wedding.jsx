e9ec0afaeac1c4e6f71a5abcbef8fb5e8100991c
JayPatel202002 <125040098+JayPatel20200@users.noreply.github.com>
2024-04-18 14:41:40 -0400

    wedding form working

--- ye-olde-walkerville/src/pages/Wedding.jsx
+++ ye-olde-walkerville/src/pages/Wedding.jsx
@@ -50,23 +50,24 @@ export const Wedding = () => {
         <h2 class="text-2xl/[29px] mb-2 pt-10">Wedding & Events Enquiry Form</h2>
         <p>Please fill out the enquiry form. We will get back to you by phone or email as soon as possible. </p>
       </div>
-              <form action="" className="p-5 lg:px-24">
+              <form action="https://api.web3forms.com/submit" method="POST" className="p-5 lg:px-24">
                 <div className="mt-1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
                   <div className="col-span-full text-lg font-medium">
+                  <input type="hidden" name="access_key" value="aa0962c5-5a44-4bd3-bb08-15cfbb2899a9" />
                     <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900 my-2">First Name</label>
                     <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border py-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600 sm:text-sm px-3" />
-                    <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900 my-2">Last Name</label>
-                    <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border py-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600 sm:text-sm px-3" />
-                    <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900 my-2">Mobile Phone</label>
-                    <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border py-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600 sm:text-sm px-3" />
-                    <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900 my-2">Email Address</label>
-                    <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border py-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600 sm:text-sm px-3" />
+                    <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900 my-2">Last Name</label>
+                    <input type="text" name="last-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border py-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600 sm:text-sm px-3" />
+                    <label htmlFor="Mobile-phone" className="block text-sm font-medium leading-6 text-gray-900 my-2">Mobile Phone</label>
+                    <input type="text" name="Mobile-phone" id="first-name" autoComplete="given-name" className="block w-full rounded-md border py-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600 sm:text-sm px-3" />
+                    <label htmlFor="Email-Address" className="block text-sm font-medium leading-6 text-gray-900 my-2">Email Address</label>
+                    <input type="text" name="Email-Address" id="first-name" autoComplete="given-name" className="block w-full rounded-md border py-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600 sm:text-sm px-3" />
                   </div>
                   {/* Repeat for other input fields */}
                 </div>
                 <div className="col-span-full mt-5">
-                  <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900 my-2">Your Equiry is</label>
-                  <textarea id="about" name="about" rows="3" className="block w-full rounded-md border py-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600 sm:text-sm px-3"></textarea>
+                  <label htmlFor="message" className="block text-sm font-medium leading-6 text-gray-900 my-2">Your Equiry is</label>
+                  <textarea id="message" name="about" rows="3" className="block w-full rounded-md border py-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600 sm:text-sm px-3"></textarea>
                   {/* <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p> */}
                 </div>
     
